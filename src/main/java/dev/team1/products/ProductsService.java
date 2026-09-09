0726ec170ed2b249370e2cf1e867b3e7fd2ed0b2
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-09 15:42:42 +0200

    GS-494: Implemented methods getAll(), getById() and getByCategory() in ProductsService

--- src/main/java/dev/team1/products/ProductsService.java
+++ src/main/java/dev/team1/products/ProductsService.java
@@ -1,5 +1,59 @@
 package dev.team1.products;
 
-public class ProductsService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dev.team1.contracts.IProductsService;
+import dev.team1.enums.ProductCategory;
+import dev.team1.mappers.ProductMapper;
+import dev.team1.products.dtos.ProductDTOResponse;
+
+@Service 
+public class ProductsService implements IProductsService {
+
+    private ProductsRepository productsRepository;
+
+    public ProductsService(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductDTOResponse> getAll() {
+        List<ProductDTOResponse> products = new ArrayList<>();
+        
+        productsRepository.findAll().forEach(p -> {
+            ProductDTOResponse dto = ProductMapper.toDTO(p);
+            products.add(dto);
+        });
+
+        return products;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ProductDTOResponse getById(Long id) {
+        ProductEntity product = productsRepository.findById(id)
+            .orElseThrow(() -> ProductExceptionNotFound("Cannot find product with id " + id + " because it doesn't exist."));
+        
+        return ProductMapper.toDTO(product);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductDTOResponse> getByCategory(ProductCategory category) {
+        List<ProductDTOResponse> products = new ArrayList<>();
+        
+        productsRepository.findByCategory(category).forEach(p -> {
+            ProductDTOResponse dto = ProductMapper.toDTO(p);
+            products.add(dto);
+        });
+
+        return products;
+    }
 
 }
