34b2ff0110e22e0f147c8889ed2f7317c6a259e2
Ioana <ioana3popescu@gmail.com>
2026-09-09 20:01:44 +0200

    feat (GS-358): add order repository

--- src/main/java/dev/team1/orders/OrdersRepository.java
+++ src/main/java/dev/team1/orders/OrdersRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrdersRepository extends JpaRepository<OrderEntity, Long> {
 
+    // Inherited CRUD methods are sufficient for creating orders for now
+
 }
