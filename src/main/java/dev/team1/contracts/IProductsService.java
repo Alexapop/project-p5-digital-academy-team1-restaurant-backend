284a51d020763b60cb0b9f361e6aeb495e654f16
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-09 14:43:04 +0200

    GS-494: Added inheritance of IGenericGetService by IProductsService and added service as argument to ProductsController.

--- src/main/java/dev/team1/contracts/IProductsService.java
+++ src/main/java/dev/team1/contracts/IProductsService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import dev.team1.enums.ProductCategory;
 import dev.team1.products.dtos.ProductDTOResponse;
 
-public interface IProductsService {
+public interface IProductsService extends IGenericGetService<ProductDTOResponse> {
 
     public List<ProductDTOResponse> getByCategory(ProductCategory category);
 
--- src/main/java/dev/team1/products/ProductsController.java
+++ src/main/java/dev/team1/products/ProductsController.java
@@ -3,9 +3,17 @@ package dev.team1.products;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.team1.contracts.IProductsService;
+
 @RestController 
 @RequestMapping(path = "${api-endpoint}/products")
 public class ProductsController {
+
+    private IProductsService productsService;
+
+    public ProductsController(IProductsService productsService) {
+        this.productsService = productsService;
+    }
 
     
 
