57805f3ba79bd75356e6f2fa782e34db05cf711c
Ioana <ioana3popescu@gmail.com>
2026-09-09 15:35:20 +0200

    feat(GS-358): add order entity

--- src/main/java/dev/team1/orders/OrderEntity.java
+++ src/main/java/dev/team1/orders/OrderEntity.java
@@ -1,5 +1,79 @@
 package dev.team1.orders;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "orders")
 public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order")
+    private Long id;
+    private BigDecimal subtotal;
+    private BigDecimal discount;
+    private BigDecimal vatRate;
+    private BigDecimal vatAmount;
+    private BigDecimal total;
+    private String chefNote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public BigDecimal getVatRate() {
+        return vatRate;
+    }
+
+    public void setVatRate(BigDecimal vatRate) {
+        this.vatRate = vatRate;
+    }
+
+    public BigDecimal getVatAmount() {
+        return vatAmount;
+    }
+
+    public void setVatAmount(BigDecimal vatAmount) {
+        this.vatAmount = vatAmount;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public String getChefNote() {
+        return chefNote;
+    }
+
+    public void setChefNote(String chefNote) {
+        this.chefNote = chefNote;
+    }
 
 }
