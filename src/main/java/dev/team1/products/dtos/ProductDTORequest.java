91d0deb065cbb37053a7115f3eb9ef0fd8df4eb1
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-09 11:52:32 +0200

    GS-493: Created ProductDTORequest and modified ProductCategory enum

--- src/main/java/dev/team1/products/dtos/ProductDTORequest.java
+++ src/main/java/dev/team1/products/dtos/ProductDTORequest.java
@@ -1,5 +1,39 @@
 package dev.team1.products.dtos;
 
-public record ProductDTORequest() {
+import java.math.BigDecimal;
+
+import dev.team1.enums.ProductCategory;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.NonNull;
+
+public record ProductDTORequest(
+    @NotBlank 
+    String name,
+
+    @NonNull 
+    ProductCategory category,
+    
+    @NotBlank 
+    String description,
+    
+    @NotBlank 
+    String imageUrl,
+    
+    @NonNull 
+    @DecimalMin(value = "0.0", inclusive = false)
+    @Digits(integer = 10, fraction = 2)
+    BigDecimal price,
+    
+    @Min(value = 0)
+    @Digits(integer = 3, fraction = 0)
+    int discount,
+    
+    boolean available,
+    
+    boolean exclusive
+) {
 
 }
