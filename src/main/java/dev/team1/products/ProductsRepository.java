b6e6f2a9c655bdf98164000715a52babb3964381
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-09 14:51:27 +0200

    GS-494: Modified query at ProductsRepository to check if product is available

--- src/main/java/dev/team1/products/ProductsRepository.java
+++ src/main/java/dev/team1/products/ProductsRepository.java
@@ -3,11 +3,13 @@ package dev.team1.products;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import dev.team1.enums.ProductCategory;
 
 public interface ProductsRepository extends JpaRepository<ProductEntity, Long>{
 
+    @Query("select p from products p where p.category = ?1 and p.available = true")
     public List<ProductEntity> findByCategory(ProductCategory category);
 
 }
