53d4c18a521525c66637d33ff3423ffd3daf5953
Ioana <ioana3popescu@gmail.com>
2026-09-10 19:56:17 +0200

    chore(GS-358): remove unused order files

--- src/main/java/dev/team1/orders/OrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.team1.orders;
-
-
-public interface OrdersRepository // extends JpaRepository<OrderEntity, Long> 
-{
-
-    // Inherited CRUD methods are sufficient for creating orders for now
-
-}
